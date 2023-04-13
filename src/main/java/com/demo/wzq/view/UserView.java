55ebfef543720350fb6669d8a818c6881518cced
YueYonghao <ofmyhub@gmail.com>
2023-04-13 16:05:35 +0800

    跨域

--- src/main/java/com/demo/wzq/view/UserView.java
+++ src/main/java/com/demo/wzq/view/UserView.java
@@ -6,6 +6,7 @@ import com.demo.wzq.uitls.JwtUtils;
 import com.demo.wzq.uitls.Log;
 import com.demo.wzq.uitls.TextUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Slf4j
 @RestController
+@CrossOrigin
 @RequestMapping("user")
 public class UserView {
 
